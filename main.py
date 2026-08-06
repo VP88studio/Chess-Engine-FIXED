cdfec0d6e34c09e9ab73472ddd7067e6e0439dba
Jimmithy <jim19946ist@gmail.com>
2026-08-06 12:46:25 +1200

    I COMMITED

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ git push origin main'''
 import pygame, math, time, sys, os
 WIDTH = 1000
 HEIGHT = 1000
-GameBoard = pygame.display.set_mode((WIDTH, HEIGHT))
+gameboard = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 running = True
 #load imgs
 chessoldimg = pygame.image.load(os.path.join("Assets/gameboard.png"))
 chessimg = pygame.transform.scale(chessoldimg, (1000, 1000))
-
+wkingoldimg = pygame.image.load(os.path.join("Pieces/white/whiteking.svg"))
+wkingimg = pygame.transform.scale(wkingoldimg, (100, 100))
 
 #chess board nested list
 
@@ -30,13 +31,13 @@ chess_board = [
 #chess board pos allocation
                         #Cords Here
 chess_board[1][0] = 13, 13
-chess_board[1][1] = 150, 950
-chess_board[1][2] = 250, 950
-chess_board[1][3] = 350, 950
-chess_board[1][4] = 450, 950
-chess_board[1][5] = 550, 950
-chess_board[1][6] = 650, 950
-chess_board[1][7] = 750, 950
+chess_board[1][1] = 138, 13
+chess_board[1][2] = 250, 13
+chess_board[1][3] = 350, 13
+chess_board[1][4] = 450, 13
+chess_board[1][5] = 550, 13
+chess_board[1][6] = 650, 13
+chess_board[1][7] = 750, 13
 print('x', chess_board[1][0][0])
 print('y', chess_board[1][0][1])
 
@@ -47,16 +48,15 @@ class pawn:
         self
 #testing area
 test_bloc = {
-    'x': chess_board[1][0][0],
-    'y': chess_board[1][0][1],
+    'x': chess_board[1][1][0],
+    'y': chess_board[1][1][1],
     'width': 100,
     'height': 100,
     'color': (0, 0, 0)
 }
-rect_test = pygame.Rect((test_bloc['x'], test_bloc['y']), (test_bloc['width'], test_bloc['height']))
 
 while running:
     
-    GameBoard.blit(chessimg, (0,0))
-    pygame.draw.rect(GameBoard, test_bloc['color'], rect_test)
+    gameboard.blit(chessimg, (0,0))
+    gameboard.blit(wkingimg, (test_bloc['x'], test_bloc['y']))
     pygame.display.flip()
